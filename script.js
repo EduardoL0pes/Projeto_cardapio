580be70c8db5a6da85ed5318ea2143f41034518b
EduardoL0pes <eduardo_lopes010@hotmail.com>
2024-04-22 17:24:15 -0300

    atualizando carrinho de produtos

--- script.js
+++ script.js
@@ -14,37 +14,64 @@ let cart = [];
 
 //Abrir Modal
 cartBtn.addEventListener('click', () => {
+    updateCartModal();
     cartModal.style.display = 'flex';
 });
 
 //Fechar Modal
 cartModal.addEventListener('click', (event) => {
-    if(event.target === cartModal || closeModalBtn) {
+    if (event.target === cartModal || closeModalBtn) {
         cartModal.style.display = 'none';
     }
 });
 
 menu.addEventListener('click', (event) => {
     let parentButton = event.target.closest('.add-to-cart-btn');
-    
-    if(parentButton) {
+
+    if (parentButton) {
         const name = parentButton.getAttribute('data-name');
         const price = parseFloat(parentButton.getAttribute('data-price'));
         addToCart(name, price);
     }
-})
+});
 
 //função para adicionar no carrinho
 function addToCart(name, price) {
     const existingItem = cart.find(item => item.name === name);
-    if(existingItem) {
+    if (existingItem) {
+        //se o item ja exite, aumenta apenas a quantidade + 1
         existingItem.quantity += 1;
-        return;
+        //senao add um novo item ao carrinho
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        });
     }
+    updateCartModal();
+}
 
-    cart.push({
-        name,
-        price,
-        quantity: 1,
+//atualiza o carrinho
+function updateCartModal() {
+    cartItemsContainer.innerHTML = '';
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement('div');
+        cartItemElement.innerHTML = `
+            <div>
+                <div>
+                    <p>${item.name}</p>
+                    <p>${item.quantity}</p>
+                    <p>${item.price}</p>
+                </div>
+                <div>
+                    <button>Remover</button>
+                </div>
+            </div>
+        `;
+
+        cartItemsContainer.appendChild(cartItemElement);
     });
 }
